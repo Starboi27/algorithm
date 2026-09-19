f8da88eaab6cb66fde25930b9593927eba56ce82
Starboi27 <gegulee2@gachon.ac.kr>
2026-09-19 14:29:15 +0900

    Clean up code by removing unused sections
    Removed commented-out code and unnecessary error handling.

--- D1/6678.py
+++ D1/6678.py
@@ -11,3 +11,17 @@ while True:
 
 # 첫 풀이 때 EOFError 처리가 없었음 떄문에 런타입 에러 발생
 # islower()에 ()을 안 쓰고 islower만 씀 검사를 안 함
+    
+# while 1:
+#     result = ""
+#     try:
+#         n = input()
+#     except EOFError:
+#         break
+#     for i in n:
+#         if i.islower():
+#             result += i.upper()
+#         else:
+#             result += i
+#     print(f">> {result}")
+# 이렇게 굳이 안 쓰고 그냥 마지막 프린트에 n.upprt() 쓰면 됨 
